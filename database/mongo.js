d6a7bb0fcafa5d73cc06d17d4ea6d618e13eec3d
yash jain <jainy2177@gmail.com>
2024-04-17 11:12:27 +0530

    change the url of mongodb

--- database/mongo.js
+++ database/mongo.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config()
 function mongodb() {
   
-  // const mongoURI = `mongodb+srv://blogChefUser:${process.env.MONGO_KEY}@cluster0.pq0gbfi.mongodb.net/?retryWrites=true&w=majority`;
+  const mongoURI = `mongodb+srv://blogChefUser:${process.env.MONGO_KEY}@cluster0.pq0gbfi.mongodb.net/?retryWrites=true&w=majority`;
 
-  const mongoURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_KEY}@cluster0.duvmrrh.mongodb.net/`;
+  // const mongoURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_KEY}@cluster0.duvmrrh.mongodb.net/`;
   mongoose
     .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
