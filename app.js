d9093ded01f6c833209350d463b805c341f01038
aniketmm <aniketmukherjee5@gmail.com>
2024-04-24 13:00:59 +0530

    changes

--- app.js
+++ app.js
@@ -148,9 +148,14 @@ app.get("/get-video", async (req, res) => {
 });
 
 app.get("/media", async (req, res) => {
+try {
   const data = await Media.find().sort({ createdAt: -1 });
 
   res.status(200).json({ data });
+} catch (error) {
+  console.log(error)
+  res.status(500).json({message: "Internal Server Error"})
+}
 });
 
 app.post("/get-existingItem", async (req, res) => {
