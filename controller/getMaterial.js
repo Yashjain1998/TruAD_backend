be19bbd4b49fb16314e7e88ab304203c72959cbe
aniketmm <aniketmukherjee5@gmail.com>
2024-04-24 12:59:13 +0530

    chnages

--- controller/getMaterial.js
+++ controller/getMaterial.js
@@ -1,7 +1,12 @@
 import Material from '../database/mongo_schema_material.js'
 async function GetMaterial(req, res){
+try {
     const materials = await Material.find();
     res.status(200).json({materials})
+} catch (error) {
+    console.log(error)
+    res.status(500).json({message: "Internal Server Error"})
 }
-
+}
+;
 export default GetMaterial
